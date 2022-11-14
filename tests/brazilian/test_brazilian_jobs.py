a9b976dad06e7925cc791ff230ffda83fd888256
AugustoMagalhaes <augustommg@gmail.com>
2022-11-14 00:59:09 -0300

    test: implementa test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
+    br_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for job in br_jobs:
+        assert "title" in job.keys()
+        assert "titulo" not in job.keys()
+        assert "salary" in job.keys()
+        assert "salario" not in job.keys()
+        assert "type" in job.keys()
+        assert "tipo" not in job.keys()
     pass
